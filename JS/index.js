fc350c87c62e3269252319ea13fdd39be8b58aaf
Mohamed Sebaa <mmohamedsebaa@gmail.com>
2019-07-01 16:33:49 +0200

    Hover on index

--- JS/index.js
+++ JS/index.js
@@ -5,15 +5,34 @@
 const celestePage = document.getElementById('celestediv');
 const personaPage = document.getElementById('personadiv');
 const zeldaPage = document.getElementById('zeldadiv');
-const celesteDim = document.querySelector('dim-videoceleste');
+const celesteDim = document.getElementById('dim-celeste');
+const personaDim = document.getElementById('dim-persona');
+const zeldaDim = document.getElementById('dim-zelda');
 
-celestePage.addEventListener('mouseenter', () => {
-    celesteDim.style.display = 'none';
+celestePage.addEventListener('mouseenter', i => {
+    celesteDim.style.opacity = '0';
 });
 
-celestePage.addEventListener('mouseleave', () => {
-    celesteDim.style.display = 'block';
+celestePage.addEventListener('mouseleave', i => {
+    celesteDim.style.opacity = '1';
 });
+
+personaPage.addEventListener('mouseenter', i => {
+    personaDim.style.opacity = '0';
+});
+
+personaPage.addEventListener('mouseleave', i => {
+    personaDim.style.opacity = '1';
+});
+
+zeldaPage.addEventListener('mouseenter', i => {
+    zeldaDim.style.opacity = '0';
+});
+
+zeldaPage.addEventListener('mouseleave', i => {
+    zeldaDim.style.opacity = '1';
+});
+
 
 
 
